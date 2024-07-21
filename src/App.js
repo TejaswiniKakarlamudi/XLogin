f99d3dfcd8d6d61f9b49f0f8e08f53b4b1f26fa6
TejaswiniKakarlamudi <kakarlamuditejaswini@gmail.com>
2024-07-21 19:32:27 +0530

    Start

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <from>
+          <input type="text" placeholder='Username'/>
+          <input type="password" placeholder='Password'/>
+          <button type='submit' className="submitted">Login</button>
+        </from>
+      </header>
+    </div>
+  );
+}
+
+export default App;
